fbfc726263ce6cfa5a85988b2f1db7fcc0800f95
Daniel Altman <Daniel.Altman4@gmail.com>
2020-10-18 00:11:43 -0700

    adding header

---
+++ empdir/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Filter by Name</p>
+        </div>
+    )
+}
+
+export default Header;
