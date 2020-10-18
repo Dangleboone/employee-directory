67df63e874e44d60131fa5ad707b61111ac1b439
Daniel Altman <Daniel.Altman4@gmail.com>
2020-10-18 00:35:43 -0700

    trying to get working

--- empdir/src/App.js
+++ empdir/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from './components/Main';
 import Wrapper from './components/Wrapper';
 import Header from './components/Header';
